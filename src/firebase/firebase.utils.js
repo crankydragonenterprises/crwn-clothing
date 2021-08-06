3f36415257b50232ff974428987ce485f599fcdd
Angela <crankydragonenterprises@gmail.com>
2021-08-06 09:01:05 -0400

    implemented firebase utils, included ability to store authenticated users into firebase database" git push origin main

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app';
-import 'firebase/database';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 const config = {
     apiKey: "AIzaSyCqy0GKFJBamiKYLZYsvJZj8O-DBJupMqo",
@@ -10,9 +10,12 @@ const config = {
     messagingSenderId: "646942967107",
     appId: "1:646942967107:web:4f61cba43ff118a7275bf6",
     measurementId: "G-JHDG03J6YN"
-  };
+  };  
 
-  firebase.initializeApp(config);
+  /* intended to prevent duplicate firebase error */
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore;
@@ -22,3 +25,30 @@ const config = {
   export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
   export default firebase;
+
+  
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    var db = firestore();
+    const userRef = db.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) /* If it doesn't exist */
+    {
+      /* Create a document */
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName, email, createdAt, ...additionalData
+        })
+      } catch (error) {
+        console.log('error creating user', error.message)
+      }
+    }
+
+    return userRef;
+  }
